0b86d26ee24559fc1f7f04990ef4d27b5538adbe
Alberto Fleitas <betofleitascardus@hotmail.com>
2023-01-13 20:35:16 -0300

    feat: calculate sale details

--- django_pos/products/models.py
+++ django_pos/products/models.py
@@ -54,4 +54,7 @@ class Product(models.Model):
         item = model_to_dict(self)
         item['id'] = self.id
         item['text'] = self.name
+        item['category'] = self.category.name
+        item['quantity'] = 1
+        item['total_product'] = 0
         return item
