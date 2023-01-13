49dc013147ad4be1d93d09265825437f91e1b860
Alberto Fleitas <betofleitascardus@hotmail.com>
2023-01-13 12:36:42 -0300

    feat: update, delete products

--- django_pos/products/urls.py
+++ django_pos/products/urls.py
@@ -8,15 +8,21 @@ urlpatterns = [
     path('categories', views.CategoriesListView, name='categories_list'),
     # Add category
     path('categories/add', views.CategoriesAddView, name='categories_add'),
-    # Delete category
-    path('categories/delete/<str:category_id>',
-         views.CategoriesDeleteView, name='categories_delete'),
     # Update category
     path('categories/update/<str:category_id>',
          views.CategoriesUpdateView, name='categories_update'),
+    # Delete category
+    path('categories/delete/<str:category_id>',
+         views.CategoriesDeleteView, name='categories_delete'),
 
     # List products
     path('', views.ProductsListView, name='products_list'),
-    # Add category
+    # Add product
     path('add', views.ProductsAddView, name='products_add'),
+    # Update product
+    path('update/<str:product_id>',
+         views.ProductsUpdateView, name='products_update'),
+    # Delete product
+    path('delete/<str:product_id>',
+         views.ProductsDeleteView, name='products_delete'),
 ]
--- django_pos/products/views.py
+++ django_pos/products/views.py
@@ -56,26 +56,6 @@ def CategoriesAddView(request):
 
 
 @login_required(login_url="/accounts/login/")
-def CategoriesDeleteView(request, category_id):
-    """
-    Args:
-        category_id : The category's ID that will be deleted
-    """
-    try:
-        # Get the category to delete
-        category = Category.objects.get(id=category_id)
-        category.delete()
-        messages.success(request, '¡Category: ' + category.name +
-                         ' deleted!', extra_tags="success")
-        return redirect('products:categories_list')
-    except Exception as e:
-        messages.success(
-            request, 'There was an error during the elimination!', extra_tags="danger")
-        print(e)
-        return redirect('products:categories_list')
-
-
-@login_required(login_url="/accounts/login/")
 def CategoriesUpdateView(request, category_id):
     """
     Args:
@@ -134,6 +114,26 @@ def CategoriesUpdateView(request, category_id):
 
 
 @login_required(login_url="/accounts/login/")
+def CategoriesDeleteView(request, category_id):
+    """
+    Args:
+        category_id : The category's ID that will be deleted
+    """
+    try:
+        # Get the category to delete
+        category = Category.objects.get(id=category_id)
+        category.delete()
+        messages.success(request, '¡Category: ' + category.name +
+                         ' deleted!', extra_tags="success")
+        return redirect('products:categories_list')
+    except Exception as e:
+        messages.success(
+            request, 'There was an error during the elimination!', extra_tags="danger")
+        print(e)
+        return redirect('products:categories_list')
+
+
+@login_required(login_url="/accounts/login/")
 def ProductsListView(request):
     context = {
         "active_icon": "products",
@@ -185,3 +185,84 @@ def ProductsAddView(request):
             return redirect('products:products_add')
 
     return render(request, "products/products_add.html", context=context)
+
+
+@login_required(login_url="/accounts/login/")
+def ProductsUpdateView(request, product_id):
+    """
+    Args:
+        product_id : The product's ID that will be updated
+    """
+
+    # Get the product
+    try:
+        # Get the product to update
+        product = Product.objects.get(id=product_id)
+    except Exception as e:
+        messages.success(
+            request, 'There was an error trying to get the product!', extra_tags="danger")
+        print(e)
+        return redirect('products:products_list')
+
+    context = {
+        "active_icon": "products",
+        "product_status": Product.status.field.choices,
+        "product": product,
+        "categories": Category.objects.all()
+    }
+
+    if request.method == 'POST':
+        try:
+            # Save the POST arguements
+            data = request.POST
+
+            attributes = {
+                "name": data['name'],
+                "status": data['state'],
+                "description": data['description'],
+                "category": Category.objects.get(id=data['category']),
+                "price": data['price']
+            }
+
+            # Check if a product with the same attributes exists
+            if product.objects.filter(**attributes).exists():
+                messages.error(request, 'Product already exists!',
+                               extra_tags="warning")
+                return redirect('products:products_add')
+
+            # Get the product to update
+            product = Product.objects.filter(
+                id=product_id).update(**attributes)
+
+            product = Product.objects.get(id=product_id)
+
+            messages.success(request, '¡Product: ' + product.name +
+                             ' updated successfully!', extra_tags="success")
+            return redirect('products:products_list')
+        except Exception as e:
+            messages.success(
+                request, 'There was an error during the update!', extra_tags="danger")
+            print(e)
+            return redirect('products:products_list')
+
+    return render(request, "products/products_update.html", context=context)
+
+
+@login_required(login_url="/accounts/login/")
+def ProductsDeleteView(request, product_id):
+    """
+    Args:
+        product_id : The product's ID that will be deleted
+    """
+    try:
+        # Get the product to delete
+        product = Product.objects.get(id=product_id)
+        product.delete()
+        messages.success(request, '¡Product: ' + product.name +
+                         ' deleted!', extra_tags="success")
+        return redirect('products:products_list')
+    except Exception as e:
+        messages.success(
+            request, 'There was an error during the elimination!', extra_tags="danger")
+        print(e)
+        return redirect('products:products_list')
